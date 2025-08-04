f7e20e4371c84e00fe116317602d565b15cc1bb7
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-08-04 14:13:25 +0900

    Update OrderSearch.java

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -1,5 +1,10 @@
-package jpabook.jpashop.domain;
+package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class OrderSearch {
 
     private String memberName; //회원 이름

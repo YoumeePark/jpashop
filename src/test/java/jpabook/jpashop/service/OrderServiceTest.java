a07d0b9c410825a34576f2a286db7125bbbe8e26
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-08-06 11:28:28 +0900

    Update OrderServiceTest.java

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -43,7 +43,7 @@ class OrderServiceTest {
         assertEquals(OrderStatus.ORDER, getOrder.getStatus(), "상품 주문시 상태는 ORDER");
         assertEquals(1, getOrder.getOrderItems().size(), "주문한 상품 종류 수가 정확해야 한다.");
         assertEquals(10000 * 2, getOrder.getTotalPrice(), "주문 가격은 가격 * 수량이다.");
-        assertEquals(8, item.getStockQuantitiy(), "주문 수량만큼 재고가 줄어야 한다.");
+        assertEquals(8, item.getStockQuantity(), "주문 수량만큼 재고가 줄어야 한다.");
     }
 
     @Test
@@ -76,7 +76,7 @@ class OrderServiceTest {
         Order getOrder = orderRepository.findOne(orderId);
 
         assertEquals(OrderStatus.CANCEL, getOrder.getStatus(), "주문 취소시 상태는 CANCEL 이다.");
-        assertEquals(10, item.getStockQuantitiy(), "주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
+        assertEquals(10, item.getStockQuantity(), "주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
     }
 
     private Member createMember() {
@@ -87,10 +87,10 @@ class OrderServiceTest {
         return member;
     }
 
-    private Book createBook(String name, int price, int stockQuantitiy) {
+    private Book createBook(String name, int price, int stockQuantity) {
         Book book = new Book();
         book.setName(name);
-        book.setStockQuantitiy(stockQuantitiy);
+        book.setStockQuantity(stockQuantity);
         book.setPrice(price);
         em.persist(book);
         return book;
